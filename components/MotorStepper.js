beea4e3cc65115a251af24d8fafce4ed2d1eaa26
kumie <kumie@nationbuilder.com>
2017-09-17 11:39:12 -0700

    added repl

--- components/MotorStepper.js
+++ components/MotorStepper.js
@@ -1,40 +1,73 @@
-// const five = require('johnny-five') ;
+const five = require('johnny-five') ;
 const temporal = require('temporal');
 const repl = require('repl');
-// const board = new five.Board();
-// const led = new five.Led();
-
+const _ = require('lodash');
+const board = new five.Board({ repl: false });
 
 class MotorStepper {
   constructor() {
-    // board.on('ready', () => {
-    //   this.stop();
-    //   this.start(nTimesToRun);
-    // });
-    this.promptForSteps();
+    board.on('ready', this.handleBoardReady.bind(this));
   }
 
-  promptForSteps() {
-    // direction
-    repl.start({ prompt: '> Number of times to step: ', eval: this.start.bind(this) });
+  handleBoardReady() {
+    const pin = this.createPin(4);
+
+    this.stop(pin);
+    this.promptForSteps(pin);
   }
 
-  start(nTimesToStep) {
-    console.log(nTimesToStep);
-    temporal.loop(10, (temporal) => {
-      if (temporal.called <= nTimesToStep) {
-        this.doStep(temporal.called);
+  createPin(position) {
+    return new five.Pin(position);
+  }
+
+  promptForSteps(pin) {
+    repl.start({
+      prompt: '> Please enter the direction and number of steps e.g. STEPCW 5\n',
+      eval: (userInput) => {
+        this.evalUserStepsInput(userInput, pin);
       }
     });
   }
 
-  stop() {
-    // led.stop().off();
+  evalUserStepsInput(userStepsInput, pin) {
+    const direction = this.getDirection(userStepsInput);
+    const numberOfSteps = this.getNumberOfSteps(userStepsInput);
+
+    if (direction && numberOfSteps) {
+      this.start({ pin, direction, numberOfSteps });
+    }
   }
 
-  doStep(nTimesRun) {
-    console.log(nTimesRun);
-    // led.pulse();
+  getDirection(userStepsInput) {
+    const directionPattern = /c(c|w)+/i;
+    const directionMatch = userStepsInput.match(directionPattern);
+
+    if (_.isArray(directionMatch)) return directionMatch[0];
+
+    return null;
+  }
+
+  getNumberOfSteps(userStepsInput) {
+    const numberOfStepsMatch = userStepsInput.match(/\d/g);
+
+    if (_.isArray(numberOfStepsMatch)) return numberOfStepsMatch.join('');
+  }
+
+  start({ pin, direction, numberOfSteps }) {
+    temporal.loop(10, (temporal) => {
+      if (temporal.called <= numberOfSteps) {
+        const lowOrHigh = temporal.called % 2 === 0 ? 'high' : 'low';
+        this.doStep({ pin, lowOrHigh });
+      }
+    });
+  }
+
+  stop(pin) {
+    pin.low();
+  }
+
+  doStep({ pin, lowOrHigh }) {
+    return pin[lowOrHigh]();
   }
 }
 
