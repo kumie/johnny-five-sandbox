1a1c6068431379b1bca11904f66973049e001dc6
kumie <kumie@nationbuilder.com>
2017-09-18 21:59:02 -0700

    repl refactor

--- components/MotorStepper.js
+++ components/MotorStepper.js
@@ -1,19 +1,26 @@
 const five = require('johnny-five') ;
 const temporal = require('temporal');
-const repl = require('repl');
 const _ = require('lodash');
-const board = new five.Board({ repl: false });
+const board = new five.Board();
 
+/**
+ * @class MotorStepper
+ * @example new MotorStepper('stepccw', 5)
+ * @param {Object} [opts= {}] - Configuration options
+ * @param {Array} [opts.pinPositions = [4, 5]] - The optional pin positions on the board.  The default
+ * positions are 4 and 5
+ *
+ */
 class MotorStepper {
-  constructor() {
-    board.on('ready', this.handleBoardReady.bind(this));
+  constructor({ pinPositions = [4, 5] } = {}) {
+    board.on('ready', this.handleBoardReady.bind(this, pinPositions));
   }
 
-  handleBoardReady() {
-    const pins = [this.createPin(4), this.createPin(5)];
+  handleBoardReady(pinPositions) {
+    const pins = _.map(pinPositions, this.createPin.bind(this));
 
     this.stop(pins);
-    this.promptForSteps(pins);
+    this.promptForCommands(pins);
   }
 
   /**
@@ -25,62 +32,33 @@ class MotorStepper {
     return new five.Pin(position);
   }
 
-  promptForSteps(pins) {
-    repl.start({
-      prompt: '> Direction and number of steps e.g. STEPCW 5\n',
-      eval: (userStepsInput) => {
-        this.evalUserStepsInputAndStart({ userStepsInput, pins });
+  /**
+   * @example CMD('stepccw', 5)
+   * @param {Array} pins
+   */
+  promptForCommands(pins) {
+    board.repl.inject({
+      CMD: (...commands) => {
+        const [stepCommand, numberOfSteps] = commands;
+        const direction = this.getDirection(stepCommand);
+
+        this.start({ pins, direction, numberOfSteps });
       }
     });
   }
 
   /**
-   * Determines the direction and number of steps the user has requested and executes them.
-   * @param {String} userStepsInput
-   * @param {Array} pins
-   */
-  evalUserStepsInputAndStart({ userStepsInput, pins }) {
-    const direction = this.getDirection(userStepsInput);
-    const numberOfSteps = this.getNumberOfSteps(userStepsInput);
-
-    if (direction && numberOfSteps) {
-      this.start({ pins, direction, numberOfSteps });
-    }
-  }
-
-  /**
    * Determines whether the direction is clock wise or counter clock wise
    * @example this.getDirection('STEPCCW') => 'ccw'
-   * @param {String} userStepsInput - The step instructions the user has entered, e.g. 'STEPCW 5'
-   * @returns {String} Returns 'cw', 'ccw', or an empty string
+   * @param {String} stepCommand - The step instructions the user has entered, e.g. 'STEPCW 5' or 'stepccw'
+   * @returns {String} e.g. 'ccw'
    */
-  getDirection(userStepsInput) {
-    const directionPattern = /c(c|w)+/i;
-    const directionMatch = userStepsInput.match(directionPattern);
-
-    if (_.isArray(directionMatch)) {
-      return _.chain(directionMatch)
-        .head()
-        .toLower()
-        .value();
-    }
-
-    return '';
-  }
-
-  /**
-   *
-   * @param userStepsInput - The step instructions the user has entered, e.g. 'STEPCW 5'
-   * @returns {Number} Returns 0 if no steps are given
-   */
-  getNumberOfSteps(userStepsInput) {
-    const numberOfStepsMatch = userStepsInput.match(/\d/g);
-
-    if (_.isArray(numberOfStepsMatch)) {
-      return Number(numberOfStepsMatch.join(''));
-    }
-
-    return 0;
+  getDirection(stepCommand) {
+    return _.chain(stepCommand)
+      .split(/step/i)
+      .last()
+      .toLower()
+      .value();
   }
 
   /**
@@ -90,9 +68,10 @@ class MotorStepper {
    * @param {Number} numberOfSteps
    */
   start({ pins, direction, numberOfSteps }) {
-    temporal.loop(10, (temporal) => {
+    temporal.loop(500, (temporal) => {
       if (temporal.called <= numberOfSteps) {
         const lowOrHigh = temporal.called % 2 === 0 ? 'high' : 'low';
+
         this.doStep({ pins, lowOrHigh, direction });
       }
     });
@@ -107,7 +86,7 @@ class MotorStepper {
    * 'ccw', both pins will be toggled.
    * @param {Array} pins
    * @param {String} direction
-   * @param {String} lowOrHigh
+   * @param {String} lowOrHigh - Whether the pins should be set to 'low' or 'high'
    */
   doStep({ pins, direction, lowOrHigh }) {
     const [pin1, pin2] = pins;
